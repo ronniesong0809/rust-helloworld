47fcd3f1b5ba04d2c4f81d1486f294f337f4189b
Ronnie Song <ronniesong0809@gmail.com>
2020-01-05 20:13:06 -0800

    fix: make it recursively

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 use std::env;
 use std::str::FromStr;
+use std::io::Write;
+use std::io;
 
 fn main() {
     let mut numbers = Vec::new();
 
     for arg in env::args().skip(1) {
-        numbers.push(u64::from_str( & arg).expect("err"));
+        numbers.push(u64::from_str( & arg).expect("err parsing"));
     }
 
-    let result = gcd(numbers[0], numbers[1]);
+    if numbers.len() == 0 {
+        writeln!(io::stderr(), "").unwrap();
+        std::process::exit(1);
+    }
 
-    println!("The gcd of {:?} is {}", numbers, result);
+    let mut a = numbers[0];
+    for b in &numbers[1..] {
+        a = gcd(a, *b);
+    }
+
+    println!("The gcd of {:?} is {}", numbers, a);
 }
 
-fn gcd(mut a: u64, mut b: u64) - > u64 {
+fn gcd(mut a: u64, mut b: u64) -> u64 {
     while b != 0 {
         if b < a {
             let temp = b;
